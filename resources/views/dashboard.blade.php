0d345b719e3889b61de26c18c07b5b997937e9a6
aliinS <aliin.sepp@gmail.com>
2024-05-10 10:33:12 +0300

    small changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,11 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{ __("Welcome to this little blog called 'Uudised'!") }}
                 </div>
                 <div class="p-6 text-gray-900">
-                    {{ __("Welcome to this little blog site!") }}
+                    {{ __("You're logged in!") }}
                 </div>
+                
             </div>
         </div>
     </div>
